8d8d8a7f1a4d8aecb3388680a56fb48f1bb4d36f
haiming.cheng <chenghm123@qq.com>
2019-04-03 15:22:16 +0800

    httpclient

--- demo-standalone/demo-standalone-other/src/test/java/com/accelerator/demo/standalone/other/MainTest.java
+++ demo-standalone/demo-standalone-other/src/test/java/com/accelerator/demo/standalone/other/MainTest.java
@@ -1,9 +1,15 @@
 package com.accelerator.demo.standalone.other;
 
+import com.accelerator.demo.standalone.other.http.HttpUtils;
+
 public class MainTest {
 
-    public static void main(String[] args) throws InterruptedException {
-
+    public static void main(String[] args) throws Exception {
+        String url = "/api/weather/city/101030100";
+        String host = "t.weather.sojson.com";
+        int port = 80;
+        HttpUtils.doGet(host, port, url);
     }
+
 
 }
