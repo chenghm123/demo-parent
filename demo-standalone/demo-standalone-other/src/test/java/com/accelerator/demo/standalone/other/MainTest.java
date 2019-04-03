6730b45fd1f761e50b67ebab2969234741780efd
haiming.cheng <chenghm123@qq.com>
2019-04-03 20:00:17 +0800

    通过Socket封装的简单HTTP工具类

--- demo-standalone/demo-standalone-other/src/test/java/com/accelerator/demo/standalone/other/MainTest.java
+++ demo-standalone/demo-standalone-other/src/test/java/com/accelerator/demo/standalone/other/MainTest.java
@@ -1,14 +1,10 @@
 package com.accelerator.demo.standalone.other;
 
-import com.accelerator.demo.standalone.other.http.HttpUtils;
 
 public class MainTest {
 
     public static void main(String[] args) throws Exception {
-        String url = "/api/weather/city/101030100";
-        String host = "t.weather.sojson.com";
-        int port = 80;
-        System.out.println(HttpUtils.doGet(host, port, url));
+
     }
 
 
