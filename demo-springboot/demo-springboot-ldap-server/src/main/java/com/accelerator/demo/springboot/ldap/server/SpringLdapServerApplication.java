9dd26d9fe0a8fc5aadd70b86d295dcd8cda8bdb3
haiming.cheng <chenghm123@qq.com>
2020-05-06 14:45:51 +0800

    LDAP模块

---
+++ demo-springboot/demo-springboot-ldap-server/src/main/java/com/accelerator/demo/springboot/ldap/server/SpringLdapServerApplication.java
@@ -0,0 +1,14 @@
+package com.accelerator.demo.springboot.ldap.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLdapServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLdapServerApplication.class, args);
+    }
+
+}
+
---
+++ demo-springboot/demo-springboot-ldap-server/src/test/java/com/accelerator/demo/springboot/ldap/DefaultTest.java
@@ -0,0 +1,63 @@
+package com.accelerator.demo.springboot.ldap;
+
+import org.junit.Test;
+import org.springframework.ldap.core.AttributesMapper;
+import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.ldap.core.support.LdapContextSource;
+import org.springframework.ldap.filter.AndFilter;
+import org.springframework.ldap.filter.EqualsFilter;
+
+import javax.naming.NamingException;
+import javax.naming.directory.Attributes;
+import java.util.List;
+import java.util.Map;
+
+public class DefaultTest {
+
+    public static LdapTemplate getLdapTemplate() {
+        LdapTemplate template = null;
+        try {
+            LdapContextSource contextSource = new LdapContextSource();
+
+            String url = "ldap://127.0.0.1:10389";
+            String base = "dc=jboss,dc=org";
+            String userDn = "uid=admin,ou=system";
+            String password = "secret";
+
+            contextSource.setUrl(url);
+            contextSource.setBase(base);
+            contextSource.setUserDn(userDn);
+            contextSource.setPassword(password);
+            contextSource.setPooled(false);
+            contextSource.afterPropertiesSet(); // important
+            template = new LdapTemplate(contextSource);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return template;
+    }
+
+    @Test
+    public void test() {
+        LdapTemplate template = getLdapTemplate();
+
+        AndFilter filter = new AndFilter();
+        filter.and(new EqualsFilter("objectClass", "organizationalPerson"));
+        filter.and(new EqualsFilter("sn", "duke"));
+        String fileCatalog = "ou=Users";
+        //                        Dn rdns = new Dn(new Rdn(fileCatalog));
+
+
+        //查询所有内部人员
+        List<Map> users = template.search(fileCatalog, filter.encode(), new AttributesMapper<Map>() {
+            @Override
+            public Map mapFromAttributes(Attributes attributes) throws NamingException {
+                return null;
+            }
+        });
+        for (Map user : users) {
+            System.out.println(user);
+        }
+    }
+
+}
