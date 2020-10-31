f053cfd5dc1e6e038bdd2657218f789a84818683
haiming.cheng <chenghm123@qq.com>
2020-10-31 11:24:17 +0800

    修正BUG

--- demo-standalone/demo-standalone-other/src/test/java/com/accelerator/demo/standalone/other/MDXTest.java
+++ demo-standalone/demo-standalone-other/src/test/java/com/accelerator/demo/standalone/other/MDXTest.java
@@ -2,10 +2,10 @@ package com.accelerator.demo.standalone.other;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
-public class MD5Test {
+public class MDXTest {
 
     public static void main(String[] args) {
-        String str = "73.25";
+        String str = "39";
 
         for (int i = 0; i < 100000000; i++) {
             str = DigestUtils.md2Hex(str);
