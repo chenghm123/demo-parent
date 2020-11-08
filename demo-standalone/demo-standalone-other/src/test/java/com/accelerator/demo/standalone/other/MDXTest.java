d2717f68d3adfd7ff3a649d55942959dafe6f2b2
haiming.cheng <chenghm123@qq.com>
2020-11-08 13:47:03 +0800

    优化代码，升级代码

--- demo-standalone/demo-standalone-other/src/test/java/com/accelerator/demo/standalone/other/MDXTest.java
+++ demo-standalone/demo-standalone-other/src/test/java/com/accelerator/demo/standalone/other/MDXTest.java
@@ -1,17 +1,9 @@
 package com.accelerator.demo.standalone.other;
 
-import org.apache.commons.codec.digest.DigestUtils;
-
 public class MDXTest {
 
     public static void main(String[] args) {
-        String str = "39";
 
-        for (int i = 0; i < 100000000; i++) {
-            str = DigestUtils.md2Hex(str);
-        }
-
-        System.out.println(str);
     }
 
 }
