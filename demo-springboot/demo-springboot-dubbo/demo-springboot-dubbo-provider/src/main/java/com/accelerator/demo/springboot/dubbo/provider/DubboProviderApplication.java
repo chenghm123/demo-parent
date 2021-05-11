10136940c3f283595398e3d6c87456e5e8a8de94
haiming.cheng <chenghm123@qq.com>
2021-05-11 09:50:45 +0800

    添加相关配置

--- demo-springboot/demo-springboot-dubbo/demo-springboot-dubbo-provider/src/main/java/com/accelerator/demo/springboot/dubbo/provider/DubboProviderApplication.java
+++ demo-springboot/demo-springboot-dubbo/demo-springboot-dubbo-provider/src/main/java/com/accelerator/demo/springboot/dubbo/provider/DubboProviderApplication.java
@@ -2,14 +2,12 @@ package com.accelerator.demo.springboot.dubbo.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DubboProviderApplication {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(DubboProviderApplication.class);
-        Thread.sleep(Long.MAX_VALUE);
     }
 
 }
